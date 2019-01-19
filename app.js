5e19ff39d16f958ccb950a22c516baf1841e89c7
getazur <45802475+getazur@users.noreply.github.com>
2019-01-19 12:55:07 +0100

    Update app.js

--- app.js
+++ app.js
@@ -12,6 +12,6 @@ app.get('/', async (req, res) => {
     res.render('home', { locations: await locations.getCachedLocations() });
 });
 
-app.listen(80);
+app.listen(8000);
 
 console.log('listening on port 8000');
