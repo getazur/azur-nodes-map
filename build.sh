cb3fb2fa0a497ff97995d34e393c9e24e9f7eb4e
getazur <45802475+getazur@users.noreply.github.com>
2019-01-19 12:22:57 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,9 @@
 chmod +x run.sh
+sduo apt-get install nodejs npm -y
 wget https://github.com/fiorix/freegeoip/releases/download/v3.2/freegeoip-3.2-linux-amd64.tar.gz
 tar -xvf freegeoip-3.2-linux-amd64.tar.gz
 rm freegeoip-3.2-linux-amd64.tar.gz
 mv freegeoip-3.2-linux-amd64 map && cd map
 chmod +x freegeoip && mv freegeoip /usr/bin
 cd ..
+npm i
