ffd6132f64d1c9d90266b7bf9df3d8129d90135a
getazur <45802475+getazur@users.noreply.github.com>
2019-01-19 12:27:37 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,10 @@
 chmod +x run.sh
-sduo apt-get install nodejs npm -y
+sudo add-apt-repository ppa:certbot/certbot -y
+sudo apt-get update
+sduo apt-get install nginx nodejs npm python-certbot-nginx -y
+rm /etc/nginx/sites-available/default
+mv default /etc/nginx/sites-available/
+service nginx restart
 wget https://github.com/fiorix/freegeoip/releases/download/v3.2/freegeoip-3.2-linux-amd64.tar.gz
 tar -xvf freegeoip-3.2-linux-amd64.tar.gz
 rm freegeoip-3.2-linux-amd64.tar.gz
